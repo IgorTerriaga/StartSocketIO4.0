babe360ab0bd04a3d54a6b89b0d489e646291c2e
Igor Terriaga <igsantos1996@gmail.com>
2024-04-18 21:32:16 -0300

    send information

--- public/chat.js
+++ public/chat.js
@@ -5,4 +5,9 @@ const urlSeacrh = new URLSearchParams(window.location.search);
 const username = urlSeacrh.get("username");
 const room = urlSeacrh.get("select_room");
 
+socket.emit("select_room", {
+    username,
+    room
+});
+
 console.log(username, room)
